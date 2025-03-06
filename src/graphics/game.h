d2442d75614f4c1aa31003d5f9cbada6b02d9213
okraus <okraus42+github@pm.me>
2025-03-06 19:25:08 +0100

    ATFATFC-7 Minilibx closes with Esc and Cross, there are no leaks, but there are leaks in SDL2, valgrind suppression file is not working yet

--- src/graphics/game.h
+++ src/graphics/game.h
@@ -1,23 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.h                                             :+:      :+:    :+:   */
+/*   game.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: okraus <okraus@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/03 17:31:13 by okraus            #+#    #+#             */
-/*   Updated: 2025/03/06 17:26:50 by okraus           ###   ########.fr       */
+/*   Created: 2025/03/06 17:16:34 by okraus            #+#    #+#             */
+/*   Updated: 2025/03/06 17:22:11 by okraus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MAIN_H
-# define MAIN_H
 
-# include <stdlib.h>
+#ifndef GAME_H
+# define GAME_H
+
+#include "defines.h"
+
+#ifdef LIB
+    #if LIB == 1
+        #include "SDL.h"
+    #else
+        #include "mlx.h"
+    #endif
+#endif
 
 // utils
 void	put_error(char const *const str);
 void	put_str(char const *const str);
-int		game(void);
 
-#endif /* MAIN_H */
+#endif /* GAME_H */
