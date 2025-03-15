949e34d90b725ad9c23f2ae1edc9df4783cfcb0b
okraus <okraus42+github@pm.me>
2025-03-15 17:11:16 +0100

    preps

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: okraus <okraus@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/08 18:31:58 by okraus            #+#    #+#             */
-/*   Updated: 2025/03/09 17:45:09 by okraus           ###   ########.fr       */
+/*   Created: 2025/03/15 17:07:47 by okraus            #+#    #+#             */
+/*   Updated: 2025/03/15 17:07:54 by okraus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,89 +16,89 @@
 #include "defines.h"
 #include "parser.h"
 
-
-
 int	parser(char *file, t_game *g)
 {
-    char f[MAX_MAP_SIZE];  // Temporary buffer to read the file
-    int fd;
-    ssize_t bytes;
+	char f[MAX_MAP_SIZE];  // Temporary buffer to read the file
+	int fd;
+	ssize_t bytes;
 
-    // Open the file in read-only mode
-    fd = open(file, O_RDONLY);
-    if (fd == -1)
-    {
-        put_error("Error opening file\n");
-        return -1;
-    }
+	// Open the file in read-only mode
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+	{
+		put_error("Error opening file\n");
+		return -1;
+	}
 
-    // Read file contents into f buffer
-    bytes = read(fd, f, sizeof(f));
-    if (bytes == -1)
-    {
-        put_error("Error reading file\n");
-        close(fd);
-        return -1;
-    }
-    if (bytes == MAX_MAP_SIZE)
-    {
-        put_error("Map file too big\n");
-        close(fd);
-        return -1;
-    }
-    // Close the file
-    close(fd);
+	// Read file contents into f buffer
+	bytes = read(fd, f, sizeof(f));
+	if (bytes == -1)
+	{
+		put_error("Error reading file\n");
+		close(fd);
+		return -1;
+	}
+	if (bytes == MAX_MAP_SIZE)
+	{
+		put_error("Map file too big\n");
+		close(fd);
+		return -1;
+	}
+	// Close the file
+	close(fd);
+	//fake split ap to 1024 lines
+	// Process the data (example: convert bytes to int values)
+	uint32_t w;
+	int i;
+	int j;
 
-    // Process the data (example: convert bytes to int values)
-    uint32_t w;
-    int i;
-    int j;
-
-    j = 0;
-    w = 0U;
-    i = 0U;
-    while (i < bytes)
-    {
-        if (f[i] == '0')
-            g->map.m[j] = M_FLOOR;
-        else if (f[i] == '1')
-            g->map.m[j] = M_WALL;
-        else if (f[i] == 'P')
-            g->map.m[j] = M_ENTRANCE;
-        else if (f[i] == 'C')
-            g->map.m[j] = M_COLLECTIBLE;
-        else if (f[i] == 'E')
-            g->map.m[j] = M_EXIT;
-        else if (f[i] == '\n')
-        {
-            g->map.height++;
-            if (g->map.width != w)
-            {
-                if (g->map.width == 0)
-                    g->map.width = w;
-                else
-                {
-                    put_error("Map line not the correct length\n");
-                    return -1;
-                }
-            }
-            w = 0U;
-            i++;
-            continue ;
-        }
-        w++;
-        i++;
-        j++;
-    }
-    if (g->map.width != w && w != 0)
-    {
-        put_error("Last map line not the correct length\n");
-        return -1;
-    }
-    if (w != 0)
-        g->map.height++;
-    //more parsing
-    return 0;
+	j = 0;
+	w = 0U;
+	i = 0U;
+	//read max width
+	//fill to max width
+	while (i < bytes)
+	{
+		if (f[i] == '0')
+			g->map.m[j] = M_FLOOR;
+		else if (f[i] == '1')
+			g->map.m[j] = M_WALL;
+		else if (f[i] == 'P')
+			g->map.m[j] = M_ENTRANCE;
+		else if (f[i] == 'C')
+			g->map.m[j] = M_COLLECTIBLE;
+		else if (f[i] == 'E')
+			g->map.m[j] = M_EXIT;
+		else if (f[i] == '\n')
+		{
+			g->map.height++;
+			if (g->map.width != w)
+			{
+				if (g->map.width == 0)
+					g->map.width = w;
+				else
+				{
+					put_error("Map line not the correct length\n");
+					return -1;
+				}
+			}
+			w = 0U;
+			i++;
+			continue ;
+		}
+		w++;
+		i++;
+		j++;
+	}
+	if (g->map.width != w && w != 0)
+	{
+		put_error("Last map line not the correct length\n");
+		return -1;
+	}
+	if (w != 0)
+		g->map.height++;
+	//more parsing
+	return 0;
 }
 
 void	print_map(t_game *g)
@@ -107,24 +107,24 @@ void	print_map(t_game *g)
 	uint32_t x;
 
 	y = 0;
-    while (y < g->map.height)
-    {
-        x = 0;
-        while (x < g->map.width)
-        {
-            if (g->map.m[y * g->map.width + x] == M_FLOOR)
-                put_str(ANSI_BG_BLUE "  " ANSI_RESET);
-            else if (g->map.m[y * g->map.width + x] == M_WALL)
-                put_str(ANSI_BG_GREEN_DARK "  " ANSI_RESET);
-            else if (g->map.m[y * g->map.width + x] == M_ENTRANCE)
-                put_str(ANSI_BG_GRAY "  " ANSI_RESET);
-            else if (g->map.m[y * g->map.width + x] == M_COLLECTIBLE)
-                put_str(ANSI_BG_GOLD "  " ANSI_RESET);
-            else if (g->map.m[y * g->map.width + x] == M_EXIT)
-                put_str(ANSI_BG_BLUE_DARK "  " ANSI_RESET);
-            x++;
-        }
-        put_str("\n");
-        y++;
-    }
+	while (y < g->map.height)
+	{
+		x = 0;
+		while (x < g->map.width)
+		{
+			if (g->map.m[y * g->map.width + x] == M_FLOOR)
+				put_str(ANSI_BG_BLUE "  " ANSI_RESET);
+			else if (g->map.m[y * g->map.width + x] == M_WALL)
+				put_str(ANSI_BG_GREEN_DARK "  " ANSI_RESET);
+			else if (g->map.m[y * g->map.width + x] == M_ENTRANCE)
+				put_str(ANSI_BG_GRAY "  " ANSI_RESET);
+			else if (g->map.m[y * g->map.width + x] == M_COLLECTIBLE)
+				put_str(ANSI_BG_GOLD "  " ANSI_RESET);
+			else if (g->map.m[y * g->map.width + x] == M_EXIT)
+				put_str(ANSI_BG_BLUE_DARK "  " ANSI_RESET);
+			x++;
+		}
+		put_str("\n");
+		y++;
+	}
 }
